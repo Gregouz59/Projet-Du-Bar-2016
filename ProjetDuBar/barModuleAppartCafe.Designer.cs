98d19c3dbb8c6584df2b9aa1d363155c96ce7a6e
Thomas CASSAR <thomas.cassar@isen-lille.fr>
2016-11-11 11:16:51 +0100

    Add argent bière

--- ProjetDuBar/barModuleAppartCafe.Designer.cs
+++ ProjetDuBar/barModuleAppartCafe.Designer.cs
@@ -29,7 +29,77 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(barModuleAppartCafe));
+            this.drinkCocaButton = new System.Windows.Forms.Button();
+            this.barmanButton = new System.Windows.Forms.Button();
+            this.drinkBeerRinceCochon = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.drinkBeer1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // drinkCocaButton
+            // 
+            this.drinkCocaButton.Location = new System.Drawing.Point(29, 107);
+            this.drinkCocaButton.Name = "drinkCocaButton";
+            this.drinkCocaButton.Size = new System.Drawing.Size(166, 25);
+            this.drinkCocaButton.TabIndex = 21;
+            this.drinkCocaButton.Text = "boire un coca (t\'es sérieux la?!)";
+            this.drinkCocaButton.UseVisualStyleBackColor = true;
+            // 
+            // barmanButton
+            // 
+            this.barmanButton.Location = new System.Drawing.Point(192, 192);
+            this.barmanButton.Name = "barmanButton";
+            this.barmanButton.Size = new System.Drawing.Size(124, 25);
+            this.barmanButton.TabIndex = 20;
+            this.barmanButton.Text = "Barman";
+            this.barmanButton.UseVisualStyleBackColor = true;
+            // 
+            // drinkBeerRinceCochon
+            // 
+            this.drinkBeerRinceCochon.Location = new System.Drawing.Point(29, 149);
+            this.drinkBeerRinceCochon.Name = "drinkBeerRinceCochon";
+            this.drinkBeerRinceCochon.Size = new System.Drawing.Size(166, 25);
+            this.drinkBeerRinceCochon.TabIndex = 19;
+            this.drinkBeerRinceCochon.Text = "boire une bière rince cochon";
+            this.drinkBeerRinceCochon.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(314, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 25);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "s\'poser à une table t\'as vu";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 25);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "grater une conso";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 25);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "danser";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // drinkBeer1
+            // 
+            this.drinkBeer1.Location = new System.Drawing.Point(74, 61);
+            this.drinkBeer1.Name = "drinkBeer1";
+            this.drinkBeer1.Size = new System.Drawing.Size(121, 25);
+            this.drinkBeer1.TabIndex = 15;
+            this.drinkBeer1.Text = "boire une bière chimay";
+            this.drinkBeer1.UseVisualStyleBackColor = true;
             // 
             // barModuleAppartCafe
             // 
@@ -37,6 +107,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.drinkCocaButton);
+            this.Controls.Add(this.barmanButton);
+            this.Controls.Add(this.drinkBeerRinceCochon);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.drinkBeer1);
             this.Name = "barModuleAppartCafe";
             this.Size = new System.Drawing.Size(539, 347);
             this.ResumeLayout(false);
@@ -44,5 +121,13 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button drinkCocaButton;
+        private System.Windows.Forms.Button barmanButton;
+        private System.Windows.Forms.Button drinkBeerRinceCochon;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button drinkBeer1;
     }
 }
